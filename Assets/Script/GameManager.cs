d9a84bb99ab5855b7eb7cd32db46562fc362b29b
pch229 <splashch@naver.com>
2022-11-22 19:05:43 +0900

    11/22
    11/22 change - timer

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -8,18 +8,53 @@ using UnityEngine.Events;
 
 public class GameManager: MonoBehaviour
 {
-    public Button Startbutton;
-    public Button Click;
+    public Button Start_button;
+    public Button Exit_button;
+    public Button Menu_Button;
+    public Button Quit_Button;
+    public GameObject MenuPanel;
+    public Button Inventory_Button;
     // Start is called before the first frame update
     void Start()
     {
-        Start_button = GameObject.Find("Start_bitton").GetComponent<Button>();
+    
+        Start_button = GetComponent<Button>();
+        Start_button.onClick.AddListener(Startclick);
+        Exit_button.onClick.AddListener(Exitclick);
+        Exit_button = GetComponent<Button>();
+        Menu_Button = GetComponent<Button>();
+        Menu_Button.onClick.AddListener(Menuclick);
 
     }
-
+    public void Startclick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void Exitclick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+    public void Menuclick()
+    {
+        Time.timeScale = 0;
+        MenuPanel.SetActive(true);
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        MenuPanel.SetActive(false);
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
     // Update is called once per frame
     void Update()
     {
-        
+     
     }
 }
